89898c9ff041cb4bc1edefcfd23f69ad386b8479
navamihegde <66194130+navamihegde@users.noreply.github.com>
2020-06-21 18:51:39 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+b = np.ones([400,400],dtype = 'uint8')*255
+
+n = int(50)
+for i in range(n,400,n*2):
+     for j in range(n,400,n*2):
+          b[j-n:j,i-n:i] = 0
+          b[j:j+n,i:i+n] = 0
+cv2.imshow('CheckerBoard 8 * 8',b)
+cv2.waitkey(0)
+cv2.destroyAllWindows()
